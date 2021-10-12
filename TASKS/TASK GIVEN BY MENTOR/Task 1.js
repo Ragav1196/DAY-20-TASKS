65350c4055e3ed076d643bc6b17a3b4132d09b34
Ragav1196 <91084155+Ragav1196@users.noreply.github.com>
2021-10-12 14:19:52 +0530

    Tasks Submitted

---
+++ TASKS/TASK GIVEN BY MENTOR/Task 1.js
@@ -0,0 +1,292 @@
+{// TO CREATE BODY FOR ALL THE FLAGS
+const flag_body = document.createElement("section");
+document.body.append(flag_body);
+
+// TO CREATE FLAG CONTAINER 1
+const each_flag = document.querySelector("section");
+const each_flag_create = document.createElement("article")
+each_flag.append(each_flag_create);
+
+// TO ADD "CLASS" TO THE FLAG CONTAINER
+const article_class = document.querySelector("article");
+article_class.setAttribute("class", "flag_container");
+
+// TO ADD IMAGE CONTAINER
+const image_container = document.querySelector(".flag_container");
+const image_container_create = document.createElement("div")
+image_container.append(image_container_create);
+
+// TO ADD CLASS FOR IMAGE CONTAINER
+const div_class = document.querySelector("div");
+div_class.setAttribute("class", "image_container")
+
+// TO ADD IMG
+const img = document.querySelector(".image_container");
+const img_create = document.createElement("img");
+img.append(img_create);
+
+const add_img = document.querySelector("img");
+add_img.setAttribute("src", "./IMAGES/Germany.png")
+add_img.setAttribute("class", "first_flag")
+
+// TO ADD FLAG DETAIL CONTAINER
+const detail_container = document.querySelector(".flag_container");
+const detail_container_create = document.createElement("div")
+detail_container.append(detail_container_create);
+
+const divV_class = document.querySelectorAll("div");
+divV_class[1].setAttribute("class", "detail_container");
+
+// TO ADD EACH FLAG DETAIL
+const each_flag_detail_container = document.querySelector(".detail_container");
+const first_each_flag_detail_container_create = document.createElement("p")
+each_flag_detail_container.append(first_each_flag_detail_container_create);
+
+const second_each_flag_detail_container_create = document.createElement("p")
+each_flag_detail_container.append(second_each_flag_detail_container_create);
+
+const third_each_flag_detail_container_create = document.createElement("p")
+each_flag_detail_container.append(third_each_flag_detail_container_create);
+
+const fourth_each_flag_detail_container_create = document.createElement("p")
+each_flag_detail_container.append(fourth_each_flag_detail_container_create);
+
+const p_class = document.querySelectorAll("p")
+p_class[0].setAttribute("class", "first_flag_detail");
+p_class[1].setAttribute("class", "second_flag_detail");
+p_class[2].setAttribute("class", "third_flag_detail");
+p_class[3].setAttribute("class", "fourth_flag_detail");
+
+const add_detail_to_first = document.querySelector(".first_flag_detail");
+add_detail_to_first.innerText = "country : Germany"
+
+const add_detail_to_second = document.querySelector(".second_flag_detail");
+add_detail_to_second.innerText = "population : 81,770,900"
+
+const add_detail_to_third = document.querySelector(".third_flag_detail");
+add_detail_to_third.innerText = "region : Europe"
+
+const add_detail_to_fourth = document.querySelector(".fourth_flag_detail");
+add_detail_to_fourth.innerText = "capital: Berlin"}
+
+
+
+
+
+
+
+
+{// TO CREATE FLAG CONTAINER 2
+const each_flag = document.querySelector("section");
+const each_flag_create = document.createElement("article")
+each_flag.append(each_flag_create);
+
+// TO ADD "CLASS" TO THE FLAG CONTAINER
+const article_class = document.querySelectorAll("article");
+article_class[1].setAttribute("class", "flag_container");
+
+// TO ADD IMAGE CONTAINER
+const image_container = document.querySelectorAll(".flag_container");
+const image_container_create = document.createElement("div")
+image_container[1].append(image_container_create);
+
+// TO ADD CLASS FOR IMAGE CONTAINER
+const div_class = document.querySelectorAll("div");
+div_class[2].setAttribute("class", "image_container")
+
+// TO ADD IMG
+const img = document.querySelectorAll(".image_container");
+const img_create = document.createElement("img");
+img[1].append(img_create);
+
+const add_img = document.querySelectorAll("img");
+add_img[1].setAttribute("src", "./IMAGES/Australia.png")
+add_img[1].setAttribute("class", "first_flag")
+
+// TO ADD FLAG DETAIL CONTAINER
+const detail_container = document.querySelectorAll(".flag_container");
+const detail_container_create = document.createElement("div")
+detail_container[1].append(detail_container_create);
+
+const divV_class = document.querySelectorAll("div");
+divV_class[3].setAttribute("class", "detail_container");
+
+// TO ADD EACH FLAG DETAIL
+const each_flag_detail_container = document.querySelectorAll(".detail_container");
+const first_each_flag_detail_container_create = document.createElement("p")
+each_flag_detail_container[1].append(first_each_flag_detail_container_create);
+
+const second_each_flag_detail_container_create = document.createElement("p")
+each_flag_detail_container[1].append(second_each_flag_detail_container_create);
+
+const third_each_flag_detail_container_create = document.createElement("p")
+each_flag_detail_container[1].append(third_each_flag_detail_container_create);
+
+const fourth_each_flag_detail_container_create = document.createElement("p")
+each_flag_detail_container[1].append(fourth_each_flag_detail_container_create);
+
+const p_class = document.querySelectorAll("p")
+p_class[4].setAttribute("class", "first_flag_detail");
+p_class[5].setAttribute("class", "second_flag_detail");
+p_class[6].setAttribute("class", "third_flag_detail");
+p_class[7].setAttribute("class", "fourth_flag_detail");
+
+const add_detail_to_first = document.querySelectorAll(".first_flag_detail");
+add_detail_to_first[1].innerText = "country : Australia"
+
+const add_detail_to_second = document.querySelectorAll(".second_flag_detail");
+add_detail_to_second[1].innerText = "population : 91,657,312"
+
+const add_detail_to_third = document.querySelectorAll(".third_flag_detail");
+add_detail_to_third[1].innerText = "region : Aus"
+
+const add_detail_to_fourth = document.querySelectorAll(".fourth_flag_detail");
+add_detail_to_fourth[1].innerText = "capital: Canberra"}
+
+
+
+
+
+
+
+
+{// TO CREATE FLAG CONTAINER 3
+const each_flag = document.querySelector("section");
+const each_flag_create = document.createElement("article")
+each_flag.append(each_flag_create);
+
+// TO ADD "CLASS" TO THE FLAG CONTAINER
+const article_class = document.querySelectorAll("article");
+article_class[2].setAttribute("class", "flag_container");
+
+// TO ADD IMAGE CONTAINER
+const image_container = document.querySelectorAll(".flag_container");
+const image_container_create = document.createElement("div")
+image_container[2].append(image_container_create);
+
+// TO ADD CLASS FOR IMAGE CONTAINER
+const div_class = document.querySelectorAll("div");
+div_class[4].setAttribute("class", "image_container")
+
+// TO ADD IMG
+const img = document.querySelectorAll(".image_container");
+const img_create = document.createElement("img");
+img[2].append(img_create);
+
+const add_img = document.querySelectorAll("img");
+add_img[2].setAttribute("src", "./IMAGES/India.png")
+add_img[2].setAttribute("class", "first_flag")
+
+// TO ADD FLAG DETAIL CONTAINER
+const detail_container = document.querySelectorAll(".flag_container");
+const detail_container_create = document.createElement("div")
+detail_container[2].append(detail_container_create);
+
+const divV_class = document.querySelectorAll("div");
+divV_class[5].setAttribute("class", "detail_container");
+
+// TO ADD EACH FLAG DETAIL
+const each_flag_detail_container = document.querySelectorAll(".detail_container");
+const first_each_flag_detail_container_create = document.createElement("p")
+each_flag_detail_container[2].append(first_each_flag_detail_container_create);
+
+const second_each_flag_detail_container_create = document.createElement("p")
+each_flag_detail_container[2].append(second_each_flag_detail_container_create);
+
+const third_each_flag_detail_container_create = document.createElement("p")
+each_flag_detail_container[2].append(third_each_flag_detail_container_create);
+
+const fourth_each_flag_detail_container_create = document.createElement("p")
+each_flag_detail_container[2].append(fourth_each_flag_detail_container_create);
+
+const p_class = document.querySelectorAll("p")
+p_class[8].setAttribute("class", "first_flag_detail");
+p_class[9].setAttribute("class", "second_flag_detail");
+p_class[10].setAttribute("class", "third_flag_detail");
+p_class[11].setAttribute("class", "fourth_flag_detail");
+
+const add_detail_to_first = document.querySelectorAll(".first_flag_detail");
+add_detail_to_first[2].innerText = "country : India"
+
+const add_detail_to_second = document.querySelectorAll(".second_flag_detail");
+add_detail_to_second[2].innerText = "population : 1,3179,984,812"
+
+const add_detail_to_third = document.querySelectorAll(".third_flag_detail");
+add_detail_to_third[2].innerText = "region : Asia"
+
+const add_detail_to_fourth = document.querySelectorAll(".fourth_flag_detail");
+add_detail_to_fourth[2].innerText = "capital: New Delhi"}
+
+
+
+
+
+
+
+
+{// TO CREATE FLAG CONTAINER 4
+const each_flag = document.querySelector("section");
+const each_flag_create = document.createElement("article")
+each_flag.append(each_flag_create);
+
+// TO ADD "CLASS" TO THE FLAG CONTAINER
+const article_class = document.querySelectorAll("article");
+article_class[3].setAttribute("class", "flag_container");
+
+// TO ADD IMAGE CONTAINER
+const image_container = document.querySelectorAll(".flag_container");
+const image_container_create = document.createElement("div")
+image_container[3].append(image_container_create);
+
+// TO ADD CLASS FOR IMAGE CONTAINER
+const div_class = document.querySelectorAll("div");
+div_class[6].setAttribute("class", "image_container")
+
+// TO ADD IMG
+const img = document.querySelectorAll(".image_container");
+const img_create = document.createElement("img");
+img[3].append(img_create);
+
+const add_img = document.querySelectorAll("img");
+add_img[3].setAttribute("src", "./IMAGES/South Korea.png")
+add_img[3].setAttribute("class", "first_flag")
+
+// TO ADD FLAG DETAIL CONTAINER
+const detail_container = document.querySelectorAll(".flag_container");
+const detail_container_create = document.createElement("div")
+detail_container[3].append(detail_container_create);
+
+const divV_class = document.querySelectorAll("div");
+divV_class[7].setAttribute("class", "detail_container");
+
+// TO ADD EACH FLAG DETAIL
+const each_flag_detail_container = document.querySelectorAll(".detail_container");
+const first_each_flag_detail_container_create = document.createElement("p")
+each_flag_detail_container[3].append(first_each_flag_detail_container_create);
+
+const second_each_flag_detail_container_create = document.createElement("p")
+each_flag_detail_container[3].append(second_each_flag_detail_container_create);
+
+const third_each_flag_detail_container_create = document.createElement("p")
+each_flag_detail_container[3].append(third_each_flag_detail_container_create);
+
+const fourth_each_flag_detail_container_create = document.createElement("p")
+each_flag_detail_container[3].append(fourth_each_flag_detail_container_create);
+
+const p_class = document.querySelectorAll("p")
+p_class[12].setAttribute("class", "first_flag_detail");
+p_class[13].setAttribute("class", "second_flag_detail");
+p_class[14].setAttribute("class", "third_flag_detail");
+p_class[15].setAttribute("class", "fourth_flag_detail");
+
+const add_detail_to_first = document.querySelectorAll(".first_flag_detail");
+add_detail_to_first[3].innerText = "country : South Korea"
+
+const add_detail_to_second = document.querySelectorAll(".second_flag_detail");
+add_detail_to_second[3].innerText = "population : 40,400,000"
+
+const add_detail_to_third = document.querySelectorAll(".third_flag_detail");
+add_detail_to_third[3].innerText = "region : Asia"
+
+const add_detail_to_fourth = document.querySelectorAll(".fourth_flag_detail");
+add_detail_to_fourth[3].innerText = "capital: Seoul"}
